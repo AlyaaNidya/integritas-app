f89089fc1285d24d11b74e7dd4d5cc48c1c7ddd9
AlyaaNidya <130289977+AlyaaNidya@users.noreply.github.com>
2026-05-29 15:47:03 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000, // Menaikkan batas peringatan menjadi 2000 KB (2 MB)
+  }
 })
